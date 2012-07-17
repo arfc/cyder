212d1348d807d36052a93cd03099af5edfa94183
Katy Huff <katyhuff@gmail.com>
2012-07-17 12:25:58 -0500

    Adds component tests to the test driver

---
+++ src/Testing/ComponentTests.cpp
@@ -0,0 +1,44 @@
+// ComponentTests.cpp
+#include <gtest/gtest.h>
+#include <dlfcn.h>
+
+#include "Component.h"
+
+using namespace std;
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+class FakeComponent : public Component {
+  public:
+    FakeComponent() : Component() {
+
+      // initialize ordinary objects
+
+      // initialize things that don't depend on the input
+    }
+
+    virtual ~FakeComponent() {
+    }
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+class ComponentTest : public ::testing::Test {
+  protected:
+    Component* test_component;
+
+    virtual void SetUp(){
+      test_component=new Component();
+    };
+    virtual void TearDown() {
+    }
+};
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
+TEST_F(ComponentTest, defaultConstructor) {
+  ASSERT_EQUAL(test_component->name(), "");
+  ASSERT_EQUAL(test_component->inner_radius(), 0);
+  ASSERT_EQUAL(test_component->outer_radius(), NULL);
+}
+
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
