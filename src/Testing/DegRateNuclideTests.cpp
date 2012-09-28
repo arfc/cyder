987753733bd6a28618dd049d9366fd6fc4f8d041
Katy Huff <katyhuff@gmail.com>
2012-09-28 10:57:18 -0500

    changed tests to take placeholder out of neumann, put new placeholder in cauchy.

--- src/Testing/DegRateNuclideTests.cpp
+++ src/Testing/DegRateNuclideTests.cpp
@@ -212,7 +212,7 @@ TEST_F(DegRateNuclideTest, transportNuclidesDR0){
   // check the boundary flux
   EXPECT_FLOAT_EQ(0, nuc_model_ptr_->cauchy_bc(u235_));
   // check the neumann bc
-  EXPECT_FLOAT_EQ(0, nuc_model_ptr_->neumann_bc(outer_radius*2,0,u235_));
+  EXPECT_FLOAT_EQ(0 , nuc_model_ptr_->neumann_bc(outer_radius*2,0,u235_));
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
@@ -222,6 +222,7 @@ TEST_F(DegRateNuclideTest, transportNuclidesDRhalf){
   EXPECT_NO_THROW(deg_rate_ptr_->set_geom(geom_));
   double expected_src = deg_rate_*test_size_;
   double expected_conc = expected_src/(nuc_model_ptr_->geom()->volume());
+  double expected_conc_w_vel = theta_*adv_vel_*expected_conc; 
   double outer_radius = nuc_model_ptr_->geom()->outer_radius();
 
   // set the degradation rate
@@ -242,9 +243,11 @@ TEST_F(DegRateNuclideTest, transportNuclidesDRhalf){
   // Dirichlet
   EXPECT_FLOAT_EQ(expected_conc, nuc_model_ptr_->dirichlet_bc(u235_));
   // Cauchy
-  EXPECT_FLOAT_EQ(theta_*adv_vel_*expected_conc, nuc_model_ptr_->cauchy_bc(u235_));
+  double expected_cauchy = 900; // @TODO fix
+  EXPECT_FLOAT_EQ(expected_cauchy, nuc_model_ptr_->cauchy_bc(u235_));
   // Neumann
-  EXPECT_FLOAT_EQ(0, nuc_model_ptr_->neumann_bc(outer_radius*2,0,u235_));
+  double expected_neumann=expected_conc/(outer_radius);
+  EXPECT_FLOAT_EQ(expected_neumann, nuc_model_ptr_->neumann_bc(outer_radius*2,0,u235_));
 
   // remove the source term offered
   CompMapPtr extract_comp = nuc_model_ptr_->source_term_bc().first.comp();
@@ -261,9 +264,10 @@ TEST_F(DegRateNuclideTest, transportNuclidesDRhalf){
   // Dirichlet
   EXPECT_FLOAT_EQ(expected_conc, nuc_model_ptr_->dirichlet_bc(u235_));
   // Cauchy
-  EXPECT_FLOAT_EQ(theta_*adv_vel_*expected_conc, nuc_model_ptr_->cauchy_bc(u235_));
+  EXPECT_FLOAT_EQ(expected_cauchy, nuc_model_ptr_->cauchy_bc(u235_));
   // Neumann 
-  EXPECT_FLOAT_EQ(0, nuc_model_ptr_->neumann_bc(outer_radius*2, 0, u235_));
+  expected_neumann=expected_conc/(outer_radius);
+  EXPECT_FLOAT_EQ(expected_neumann, nuc_model_ptr_->neumann_bc(outer_radius*2, 0, u235_));
 
   // remove the source term offered
   extract_comp = nuc_model_ptr_->source_term_bc().first.comp();
@@ -292,6 +296,7 @@ TEST_F(DegRateNuclideTest, transportNuclidesDR1){
   EXPECT_NO_THROW(deg_rate_ptr_->set_geom(geom_));
   double expected_src = deg_rate_*test_size_;
   double expected_conc = expected_src/(nuc_model_ptr_->geom()->volume());
+  double expected_conc_w_vel = theta_*adv_vel_*expected_conc; 
   double outer_radius = nuc_model_ptr_->geom()->outer_radius();
 
   // set the degradation rate
@@ -312,9 +317,11 @@ TEST_F(DegRateNuclideTest, transportNuclidesDR1){
   // Dirichlet
   EXPECT_FLOAT_EQ(expected_conc, nuc_model_ptr_->dirichlet_bc(u235_));
   // Cauchy
-  EXPECT_FLOAT_EQ(theta_*adv_vel_*expected_conc, nuc_model_ptr_->cauchy_bc(u235_));
+  double expected_cauchy = 900; // @TODO fix
+  EXPECT_FLOAT_EQ(expected_cauchy, nuc_model_ptr_->cauchy_bc(u235_));
   // Neumann 
-  EXPECT_FLOAT_EQ(0, nuc_model_ptr_->neumann_bc(outer_radius*2, 0, u235_));
+  double expected_neumann=expected_conc/(outer_radius);
+  EXPECT_FLOAT_EQ(expected_neumann, nuc_model_ptr_->neumann_bc(outer_radius*2, 0, u235_));
 
   // remove the source term offered
   CompMapPtr extract_comp = nuc_model_ptr_->source_term_bc().first.comp();
