1f9c2a4f6113a19bb3b4d437ff9d4f69425151e1
Katy Huff <katyhuff@gmail.com>
2012-12-04 10:57:40 -0600

    reduces test scope to functions already written.

--- src/Testing/MaterialDBTests.h
+++ src/Testing/MaterialDBTests.h
@@ -1,19 +1,20 @@
 // MaterialDBTests.h
 #include <gtest/gtest.h>
-#include <MatDataTable.h>
+#include "MaterialDB.h"
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
 class MaterialDBTest : public ::testing::Test {
   protected:
     std::vector<std::string> mat_names_;
     std::vector<int> elem_ids_;
+    int u_, am_, th_, pb_, one_g_; 
 
     virtual void SetUp(){
       // composition set up
-      u235_ = 92235;
-      am241_ = 95241;
-      th228_ = 90228;
-      pb208_ = 82208;
+      u_ = 92;
+      am_ = 95;
+      th_ = 90;
+      pb_ = 82;
       one_g_ = 1.0;
 
     }
