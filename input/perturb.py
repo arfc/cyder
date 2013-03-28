e1274badc3cdbfcc571bb697f19b72f5ea321030
Katy Huff <katyhuff@gmail.com>
2013-03-28 12:06:54 -0500

    fixed broken replace.

--- input/perturb.py
+++ input/perturb.py
@@ -18,7 +18,7 @@ def configure_infile(xml_in, xml, param, val) :
     f_old = open(xml_in, 'r') 
     f_new = open(xml, 'w') 
     old = "<"+param+">[^<]*</"+param+">"
-    new = "<"+param+">[^<]"+str(val)+"</"+param+">"
+    new = "<"+param+">"+str(val)+"</"+param+">"
     for line in f_old :
         f_new.write(re.sub(old, new, line))
     f_old.close()
