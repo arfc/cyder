7e405fdce5214f5a96cc9c9fcc8ad7ecdfa7e0ff
Katy Huff <katyhuff@gmail.com>
2013-04-05 10:42:06 -0500

    modularity is key

--- output/contour_plot.py
+++ output/contour_plot.py
@@ -83,8 +83,8 @@ class ContourPlot(object) :
         self._yi = get_yi(200, self._y_min, self._y_max)
         self._zi = get_zi()
         
-        _title = get_title(_npts)
-        _filename = get_filename()
+        self._title = get_title(_npts)
+        self._filename = get_filename()
         
         self.grid_data_and_contour()
         self.plot_tricontour()
@@ -104,6 +104,17 @@ class ContourPlot(object) :
 
     def plot_tricontour(self):
         # tricontour.
+        x=self._x 
+        y=self._y 
+        z=self._z
+        n_labels=self._n_labels
+        zi=self._zi
+        x_max=self._x_max 
+        y_max=self._y_max
+        title=self._title
+        plot_tricontour(x,y,z,n_labels,x_min, x_max, y_min, y_max, zi, title)
+
+    def plot_tricontour(x,y,z,n_labels,x_min, x_max, y_min, y_max, zi, title)
         plt.subplot(212)
         triang = tri.Triangulation(_x, _y)
         plt.tricontour(_x, _y, _z, _n_labels, linewidths=0.5, colors='k')
@@ -117,8 +128,8 @@ class ContourPlot(object) :
         print ('tricontour plotted')
 
     def save_it(self) :
-        savefig(_filename)
-        print ('saved as '+_filename)
+        savefig(self._filename)
+        print ('saved as '+ self._filename)
 
     def set_x(self) :
         if self._x is not None :
@@ -134,19 +145,20 @@ class ContourPlot(object) :
         return self._xi
 
     def set_y(self, y_min, y_max, npts) :
-        if self._xi is not None :
+        if self._x is not None :
             seed(0)
             # this should be a matrix of identical columns
             self._y = uniform(self._y_min, self._y_max, _npts) 
         return self._y
 
     def set_yi(self, ngridy) :
-        spacing = 1./ngridy
-        self._yi = np.linspace(self._y_min-spacing, self._y_max+spacing, ngridy)
+        if self._yi is not None :
+            spacing = 1./ngridy
+            self._yi = np.linspace(self._y_min-spacing, self._y_max+spacing, ngridy)
         return self._yi
 
     def set_z(self) :
-        if self._xi is not None :
+        if self._z is not None :
             seed(0)
             # this should be the mass in whatever component
             self._z = self._x*np.exp(-self._x**2-self._y**2)  
