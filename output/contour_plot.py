a3d9dedaa6a36faa8cd580f5464a3ce02ec3bc78
Katy Huff <katyhuff@gmail.com>
2013-04-05 13:03:54 -0500

    plotting and ready for data.

--- output/contour_plot.py
+++ output/contour_plot.py
@@ -80,10 +80,11 @@ class ContourPlot(object) :
         self._y = self.set_y()
         self._z = self.set_z()
         self._xi = self.set_xi(100)
-        self._yi = self.set_yi(200)
+        self._yi = self.set_yi(100, 200)
+        self._zi = self.set_zi()
         
-        self._title = get_title(self._npts)
-        self._filename = get_filename()
+        self._title = self.set_title(self._npts)
+        self._filename = self.set_filename()
         
         self.grid_data_and_contour()
         self.plot_tricontour()
@@ -98,20 +99,19 @@ class ContourPlot(object) :
         xi=self._xi
         yi=self._yi
         zi=self._zi
+        x_min=self._x_min 
+        y_min=self._y_min
         x_max=self._x_max 
         y_max=self._y_max
-        grid_data_and_contour(x,y,z,xi,yi,n_labels,zi,x_min,x_max,y_min_y_max)
 
-    def grid_data_and_contour(x,y,z,xi,yi,n_labels,zi,x_min,x_max,y_min_y_max):
         plt.subplot(211)
-        zi = griddata(x, y, z, xi, yi, interp='linear')
         plt.contour(xi, yi, zi, n_labels, linewidths=0.5, colors='k')
         plt.contourf(xi, yi, zi, n_labels, cmap=plt.cm.rainbow,
                      norm=plt.normalize(vmax=abs(zi).max(), vmin=-abs(zi).max()))
         plt.colorbar() # draw colorbar
         plt.plot(x, y, 'ko', ms=3)
-        plt._xlim(x_min, x_max)
-        plt._ylim(y_min, y_max)
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
         print ('griddata and contour plotted')
 
     def plot_tricontour(self):
@@ -121,21 +121,21 @@ class ContourPlot(object) :
         z=self._z
         n_labels=self._n_labels
         zi=self._zi
+        x_min=self._x_min 
+        y_min=self._y_min
         x_max=self._x_max 
         y_max=self._y_max
         title=self._title
-        plot_tricontour(x,y,z,n_labels,x_min, x_max, y_min, y_max, zi, title)
 
-    def plot_tricontour(x,y,z,n_labels,x_min, x_max, y_min, y_max, zi, title):
         plt.subplot(212)
         triang = tri.Triangulation(x, y)
         plt.tricontour(x, y, z, n_labels, linewidths=0.5, colors='k')
-        plt.tricontourf(x, y, z, n_lables, cmap=plt.cm.rainbow,
+        plt.tricontourf(x, y, z, n_labels, cmap=plt.cm.rainbow,
                         norm=plt.normalize(vmax=abs(zi).max(), vmin=-abs(zi).max()))
         plt.colorbar()
         plt.plot(x, y, 'ko', ms=3)
-        plt._xlim(x_min, x_max)
-        plt._ylim(y_min, y_max)
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
         plt.title(title)
         print ('tricontour plotted')
 
@@ -144,45 +144,48 @@ class ContourPlot(object) :
         print ('saved as '+ self._filename)
 
     def set_x(self) :
-        if self._x is not None :
+        if self._x is None :
             seed(0)
             # this should be a matrix of identical columns?
             self._x = uniform(self._x_min, self._x_max, self._npts) 
         return self._x;
 
     def set_xi(self, ngridx) :
-        if self._xi is not None :
-            spacing = 1./ngridx
+        if self._xi is None :
+            spacing = 10./ngridx
             self._xi = np.linspace(self._x_min-spacing, self._x_max+spacing, ngridx)
         return self._xi
 
     def set_y(self) :
-        if self._x is not None :
-            seed(0)
+        if self._y is None :
             # this should be a matrix of identical columns
             self._y = uniform(self._y_min, self._y_max, self._npts) 
         return self._y
 
-    def set_yi(self, ngridy) :
-        if self._yi is not None :
-            spacing = 1./ngridy
+    def set_yi(self, ngridx, ngridy) :
+        if self._yi is None :
+            spacing = 10./ngridx
             self._yi = np.linspace(self._y_min-spacing, self._y_max+spacing, ngridy)
         return self._yi
 
     def set_z(self) :
-        if self._z is not None :
-            seed(0)
+        if self._z is None :
             # this should be the mass in whatever component
-            self._z = self._x*np.exp(-self._x**2-self._y**2)  
+            self._z = self._x*np.exp(-self._x**2 - self._y**2)  
         return self._z;
 
-    def title(npts) :
-        title = 'tricontour (%d points)' % npts
-        return title
+    def set_zi(self) :
+        if self._zi is None :
+            self._zi = griddata(self._x, self._y, self._z, self._xi, self._yi, interp='linear')
+        return self._zi
 
-    def filename() : 
-        filename = 'contour_plot.eps'
-        return filename
+    def set_title(self, npts) :
+        self._title = 'tricontour (%d points)' % npts
+        return self._title
+
+    def set_filename(self) : 
+        self._filename = 'contour_plot.eps'
+        return self._filename
 
 
 if __name__=="__main__" :
