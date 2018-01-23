72c4ba60261a5be9d3177a3e5233345c182ef20d
Jin Whan Bae <jbae11@illinois.edu>
2018-01-23 11:06:16 -0600

    Cycamore -> Recycle

---
+++ src/recycle.h
@@ -0,0 +1,12 @@
+#ifndef RECYCLE_SRC_RECYCLE_H_
+#define RECYCLE_SRC_RECYCLE_H_
+
+// These includes must come before others.
+#include "cyclus.h"
+#include "recycle_version.h"
+
+#include "corrm.h"
+
+#if CYCLUS_HAS_COIN
+
+#endif  // RECYCLE_SRC_RECYCLE_H_
