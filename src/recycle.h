8acb36f2ae76d4d9d9894d0e608e0ab729c32c1e
Jin Whan Bae <jbae11@illinois.edu>
2018-02-10 11:45:10 -0600

    added back the cycamore archetypes

--- src/recycle.h
+++ src/recycle.h
@@ -1,12 +1,22 @@
 #ifndef RECYCLE_SRC_RECYCLE_H_
 #define RECYCLE_SRC_RECYCLE_H_
 
-// These includes must come before others.
-#include "cyclus.h"
 #include "recycle_version.h"
 
-#include "corrm.h"
-
-#if CYCLUS_HAS_COIN
+#include "batch_reactor.h"
+#include "batch_reactor_tests.h"
+#include "deploy_inst.h"
+#include "enrichment.h"
+#include "enrichment_tests.h"
+#include "growth_region.h"
+#include "growth_region_tests.h"
+#include "inpro_reactor.h"
+#include "inpro_reactor_tests.h"
+#include "manager_inst.h"
+#include "manager_inst_tests.h"
+#include "sink.h"
+#include "sink_tests.h"
+#include "source.h"
+#include "source_tests.h"
 
 #endif  // RECYCLE_SRC_RECYCLE_H_
