9d99e281ee30cca2b7aaf060fc0b3d7948bf9052
gwenchee <gwendolynchee95@gmail.com>
2018-07-18 17:40:53 -0500

    changing recycle to cyder

---
+++ src/cyder.h
@@ -0,0 +1,6 @@
+#ifndef CYDER_SRC_CYDER_H_
+#define CYDER_SRC_CYDER_H_
+
+#include "cyder_version.h"
+
+#endif  // CYDER_SRC_CYDER_H_
--- src/recycle.h
+++
@@ -1,6 +0,0 @@
-#ifndef RECYCLE_SRC_RECYCLE_H_
-#define RECYCLE_SRC_RECYCLE_H_
-
-#include "recycle_version.h"
-
-#endif  // RECYCLE_SRC_RECYCLE_H_
