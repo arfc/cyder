3c7d0ccb89776d84b03f1574807742f0e0c9f484
Gwendolyn Chee <gwendolynchee95@gmail.com>
2018-07-19 10:36:17 -0500

    add condition fac

--- src/cyder.h
+++ src/cyder.h
@@ -1,6 +1,10 @@
 #ifndef CYDER_SRC_CYDER_H_
 #define CYDER_SRC_CYDER_H_
 
+#include "cyclus.h"
 #include "cyder_version.h"
+
+#include "condition.h"
+#include "condition_tests.h"
 
 #endif  // CYDER_SRC_CYDER_H_
